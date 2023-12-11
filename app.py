ec5dded04253a8dbbe9576f1bf14bbd15518b0b1
travismurphyau <153459720+travismurphyau@users.noreply.github.com>
2023-12-11 00:44:21 +0000

    commited changes

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-# import module
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-# Title
-st.title("Hello DasherBoard Users !!!")
-st.write("Hello DasherBoard ddd !!!")
+# Generate a time series for the dates
+dates = pd.date_range(start="2023-01-01", end="2023-12-31", freq='D')
+
+# Simulate toy production counts
+production_counts = np.random.randint(100, 1000, size=len(dates))
+
+# Create a DataFrame
+data = pd.DataFrame({'Date': dates, 'Toys_Produced': production_counts})
+
+# Calculate total production
+total_production = data['Toys_Produced'].sum()
+
+# Streamlit app layout
+st.title('Santa Toy Production Dashboard')
+
+# Display total production as a metric
+st.metric(label="Total Toys Produced", value=f"{total_production:,}")
+
+# Display the data as a table
+st.write("Toy Production Data:")
+st.dataframe(data)
+
+# Create a line chart
+st.line_chart(data.set_index('Date'))
