e7730a0ec45fe743209ad52441310c4787bc7772
travismurphyau <153459720+travismurphyau@users.noreply.github.com>
2023-12-11 12:01:32 +1100

    Update app.py
    yep

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-# Generate a time series for the dates
-dates = pd.date_range(start="2023-01-01", end="2023-12-31", freq='D')
-
-# Simulate toy production counts
-production_counts = np.random.randint(100, 1000, size=len(dates))
-
-# Create a DataFrame
-data = pd.DataFrame({'Date': dates, 'Toys_Produced': production_counts})
+# Load data from CSV file
+data = pd.read_csv('santa_toy_data.csv')
+data['Date'] = pd.to_datetime(data['Date'])  # Ensure 'Date' is in datetime format
 
 # Calculate total production
 total_production = data['Toys_Produced'].sum()
@@ -26,3 +20,4 @@ st.dataframe(data)
 
 # Create a line chart
 st.line_chart(data.set_index('Date'))
+
